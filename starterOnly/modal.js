ae2a75da18744d21f9a6d1f2739d7273d4be2c9d
sarkande <aperezgo74@gmail.com>
2022-03-13 18:52:08 +0100

    Issue #2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,12 @@
+
+const regFirstName = new RegExp('[a-zA-Z]{2}.*');
+const regLastName = new RegExp('[a-zA-Z]{2}.*');
+const regEmail = new RegExp('^[^@\s]+@[^@\s]+$');
+const regBirth = new RegExp('[1-2]{1}[0-9]{3}-[0-9]{2}-[0-9]{2}');
+
+
+
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -25,4 +34,49 @@ function launchModal() {
   })
 }
 
+let checkDataForm = (data)=>{
+  if(data.first === null || !regFirstName.test(data.first))
+    return false;
+  else if(data.last === null || !regLastName.test(data.last))
+    return false;
+  else if(data.mail === null || !regEmail.test(data.mail))
+    return false;
+  else if(data.birth === null || !regBirth.test(data.birth))
+    return false;
+  else if (data.turnamentCounter === null || Number.parseInt(data.turnamentCounter) === NaN)
+  {
+    console.log(Number.parseInt(data.turnamentCounter));
+    return false;
+  }
+    
+  else if (data.turnamentSelection === null)
+    return false;
+  else if (data.cgu === false)
+    return false;
+  else
+    return true;
 
+}
+
+let validate = (event) =>{
+  event.preventDefault();
+
+  let data={
+    first: document.querySelector('#first')?.value ?? null ,
+    last: document.querySelector('#last')?.value ?? null,
+    mail: document.querySelector('#email')?.value ?? null,
+    birth: document.querySelector('#birthdate')?.value ?? null,
+    turnamentCounter: document.querySelector('#quantity')?.value ?? null,
+    turnamentSelection: document.querySelector('input[name="location"]:checked')?.id ?? null,
+    cgu: document.querySelector('input#checkbox1')?.checked ?? null,
+    newsletter: document.querySelector('input#checkbox2')?.checked ?? null
+  }
+  
+  console.log(data);
+
+  if(checkDataForm(data))
+    event.currentTarget.submit();
+    else
+    console.error("Probleme de validation de donnée");
+  console.log("send");
+}
